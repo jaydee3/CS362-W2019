b2ea5ea2ab387f1ad09acf54e92244bbb36f9970
Joshua Nutt <nuttj@flip2.engr.oregonstate.edu>
2019-01-20 19:19:46 -0800

    Refactored adventurer

--- projects/nuttj/dominion/dominion.c
+++ projects/nuttj/dominion/dominion.c
@@ -656,7 +656,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   int tributeRevealedCards[2] = {-1, -1};
   int temphand[MAX_HAND];// moved above the if statement
   int drawntreasure=0;
-  int cardDrawn;
+  //int cardDrawn;
   int z = 0;// this is the counter for the temp hand
   if (nextPlayer > (state->numPlayers - 1)){
     nextPlayer = 0;
@@ -667,7 +667,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   switch( card ) 
     {
     case adventurer:
-      return adventurerEffect(state, &drawntreasure, temphand, &cardDrawn, &z, currentPlayer);
+      return adventurerEffect(state, drawntreasure, temphand, z, currentPlayer);
 			
     case council_room:
       //+4 Cards
@@ -1310,25 +1310,27 @@ int updateCoins(int player, struct gameState *state, int bonus)
   return 0;
 }
 
-int adventurerEffect(struct gameState *state, int *drawntreasure, int *temphand, int *cardDrawn, int *z, int currentPlayer)
+int adventurerEffect(struct gameState *state, int drawntreasure, int *temphand, int z, int currentPlayer)
 {
-   while(*drawntreasure<2){
+   int cardDrawn;
+
+   while(drawntreasure<2){
 	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
 	  shuffle(currentPlayer, state);
 	}
 	drawCard(currentPlayer, state);
-	*cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (*cardDrawn == copper || *cardDrawn == silver || *cardDrawn == gold)
-	  (*drawntreasure)++;
+	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
+	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+	  drawntreasure++;
 	else{
-	  temphand[*z]=*cardDrawn;
+	  temphand[z]=cardDrawn;
 	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  (*z)++;
+	  (z)++;
 	}
       }
-      while(*z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[*z-1]; // discard all cards in play that have been drawn
-	*z=*z-1;
+      while(z-1>=0){
+	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
+	z=z-1;
       }
       return 0;
 }
--- projects/nuttj/dominion/dominion.h
+++ projects/nuttj/dominion/dominion.h
@@ -128,5 +128,5 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
-int adventurerEffect(struct gameState *state, int *drawntreasure, int *temphand, int *cardDrawn, int *z, int currentPlayer);
+int adventurerEffect(struct gameState *state, int drawntreasure, int *temphand, int z, int currentPlayer);
 #endif
