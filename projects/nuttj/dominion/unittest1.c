375d9382cae5d6d40158244d38dd729dbd829690
Joshua Nutt <nuttj@flip3.engr.oregonstate.edu>
2019-02-13 21:01:20 -0800

    unittest1 & 2 git push

--- projects/nuttj/dominion/unittest1.c
+++ projects/nuttj/dominion/unittest1.c
@@ -1,5 +1,5 @@
 /***************************************************************************************
- * Unit test for isGameOver() function
+ * Unit test for supplyCount() function
  **************************************************************************************/
 #include "dominion.h"
 #include "dominion_helpers.h"
@@ -14,41 +14,59 @@ void testResult(int result, int expectedResult, char* message);
 int main()
 {
 	int k[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall};
-	struct gameState G, S; //G is the test game state, S = the default game state that is used to reset G after testing
-	int i; //counter
+	struct gameState G; //G is the test game state
 	int result; //holds result of tested functions
-	char buffer[100]; //holds failure messages
+	int x[] = {-1, 0, 3, 5, 6, 7, 10, 11, 13, 14}; //holds supply count to be assigned to cards	
 
-	printf("Performing unit test for isGameOver()\n");
+	printf("Performing unit test for supplyCount()\n");
 	
 	//initialize game (numplayers, kingdomCards[10], randomSeed, gamestate)
-	result = initializeGame(2, k, 100, &S);
+	result = initializeGame(2, k, 100, &G);
 	testResult(result, 0, "Function initializedGame failed");
 
-	//Test that the newly initialized game is not over
-	result = isGameOver(&S); //should return 0 to indicate game is not over
-	testResult(result, 0, "Test to see if game was initialized to Game Over state. Test failed.");
-	
-	//Test if the empty Province supply causes the game to end
-	memcpy(&G, &S, sizeof(S)); //copy the initialized game state to a test game state
-	G.supplyCount[province] = 0; //set Province supply to 0
-	result = isGameOver(&G); //should return 1 to indicate game is over
-	testResult(result, 1, "Test to see if empty Province Supply causes Game Over. Test failed.");
-	
-	//Test if three empty supply piles causes the game to end
-	memcpy(&G, &S, sizeof(S)); //copy the initialized game state to a test game state
-	for (i = 1; i <= 2; i++) //test to see if 1 and 2 empty supply piles causes Game Over
-	{
-		G.supplyCount[i] = 0; //set supply to 0
-		result = isGameOver(&G); 
-		sprintf(buffer, "Test to see if %d Empty supply piles causes Game Over. Test failed.", i);
-		testResult(result, 0, buffer); //isGameOver should return 0 to indicate game is not over
-	}
-	//test to see if 3 empty supply piles causes Game Over
-		G.supplyCount[i] = 0; //set supply to 0. i will be currently equal to 3
-		result = isGameOver(&G); 
-		sprintf(buffer, "Test to see if %d Empty supply piles causes Game Over. Test failed.", i);
-		testResult(result, 1, buffer); //isGameOver should return 1 to indicate game is over
+	//Set supply count for each card
+	G.supplyCount[adventurer] = x[0];
+	G.supplyCount[council_room] = x[1];
+	G.supplyCount[feast] = x[2];
+	G.supplyCount[gardens] = x[3];
+	G.supplyCount[mine] = x[4];
+	G.supplyCount[remodel] = x[5];
+	G.supplyCount[smithy] = x[6];
+	G.supplyCount[village] = x[7];
+	G.supplyCount[baron] = x[8];
+	G.supplyCount[great_hall] = x[9];
+	G.supplyCount[province] = x[0];
+	G.supplyCount[gold] = x[1];
+
+	//test all kingdom cards
+	result = supplyCount(adventurer, &G);
+	testResult(result, x[0], "Testing supply count for adventurer. Test Failed");
+	result = supplyCount(council_room, &G);
+	testResult(result, x[1], "Testing supply count for council_room. Test Failed");
+	result = supplyCount(feast, &G);
+	testResult(result, x[2], "Testing supply count for feast. Test Failed");
+	result = supplyCount(gardens, &G);
+	testResult(result, x[3], "Testing supply count for gardens. Test Failed");
+	result = supplyCount(mine, &G);
+	testResult(result, x[4], "Testing supply count for mine. Test Failed");
+	result = supplyCount(remodel, &G);
+	testResult(result, x[5], "Testing supply count for remodel. Test Failed");
+	result = supplyCount(smithy, &G);
+	testResult(result, x[6], "Testing supply count for smithy. Test Failed");
+	result = supplyCount(village, &G);
+	testResult(result, x[7], "Testing supply count for village. Test Failed");
+	result = supplyCount(baron, &G);
+	testResult(result, x[8], "Testing supply count for baron. Test Failed");
+	result = supplyCount(great_hall, &G);
+	testResult(result, x[9], "Testing supply count for great_hall. Test Failed");
+	//Test some base cards
+	result = supplyCount(province, &G);
+	testResult(result, x[0], "Testing supply count for province. Test Failed");
+	result = supplyCount(gold, &G);
+	testResult(result, x[1], "Testing supply count for gold. Test Failed");
+	//Test a kingdom card that have not been selected for the game
+	result = supplyCount(steward, &G); //cards not in game are initialized to -1
+	testResult(result, -1, "Testing supply count for steward. Test Failed");
 
 	printf("All tests passed\n");
 
